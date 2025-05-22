addc6af5420cedde66ce8a0272442f004f1acdec
sujan <sujangurung10@gmail.com>
2025-05-22 15:41:20 +0100

    added dummy key for liniting

--- next.config.ts
+++ next.config.ts
@@ -2,5 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
 
+process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY || 'dummy-key';
+process.env.STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || 'dummy-key';
+
 
 export default nextConfig;
