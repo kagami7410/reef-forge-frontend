218983d0a722ea44c0210ad0cd9554d56150ea3a
sujan <sujangurung10@gmail.com>
2025-05-26 11:58:16 +0100

    added console message for secret injection

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,29 @@
 import fs from 'fs';
 import type { NextConfig } from 'next';
 
-if(fs.existsSync("vault/secrets/stripe-api-key.txt") && fs.existsSync("vault/secrets/stripe-api-secret.txt")){
-const stripeApiKey = fs.readFileSync('vault/secrets/stripe-api-key.txt', 'utf-8').trim();
-const stripeApiSecret = fs.readFileSync('vault/secrets/stripe-api-secret.txt', 'utf-8').trim();
+if (fs.existsSync("vault/secrets/stripe-api-key.txt") && fs.existsSync("vault/secrets/stripe-api-secret.txt")) {
+  const stripeApiKey = fs.readFileSync('vault/secrets/stripe-api-key.txt', 'utf-8').trim();
+  const stripeApiSecret = fs.readFileSync('vault/secrets/stripe-api-secret.txt', 'utf-8').trim();
 
-process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY = stripeApiKey;
-process.env.STRIPE_SECRET_KEY = stripeApiSecret;
+  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY = stripeApiKey;
+  process.env.STRIPE_SECRET_KEY = stripeApiSecret;
+  if (stripeApiKey.includes("test")) {
+    console.log("Secret Injected sucessfully!");
+  }
+} else {
+  console.log("Secret not Injected!")
 }
+
 
 
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   // You can optionally expose non-sensitive env vars here
-//   env: {
-//     // Not secure for secrets!
-//     PUBLIC_SOME_KEY: 'example',
-//   },
+  //   env: {
+  //     // Not secure for secrets!
+  //     PUBLIC_SOME_KEY: 'example',
+  //   },
 };
 
 export default nextConfig;
