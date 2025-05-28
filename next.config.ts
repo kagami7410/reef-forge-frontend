9f838b4828416fd2a9c559e3f4f5108cfa844d66
sujan <sujangurung10@gmail.com>
2025-05-28 22:52:15 +0100

    testing env var in client

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import fs from 'fs';
 import type { NextConfig } from 'next';
 
+
+let stripeApiKey;
+let stripeApiSecret;
+
 if (fs.existsSync("vault/secrets/stripe-api-key.txt") && fs.existsSync("vault/secrets/stripe-api-secret.txt")) {
   const stripeApiKey = fs.readFileSync('vault/secrets/stripe-api-key.txt', 'utf-8').trim();
   const stripeApiSecret = fs.readFileSync('vault/secrets/stripe-api-secret.txt', 'utf-8').trim();
@@ -30,8 +34,8 @@ const nextConfig: NextConfig = {
   // You can optionally expose non-sensitive env vars here
     env: {
       // Not secure for secrets!
-      NEXT_PUBLIC_STRIPE_PUBLIC_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
-      STRIPE_SECRET_KEY:  process.env.STRIPE_SECRET_KEY
+      NEXT_PUBLIC_STRIPE_PUBLIC_KEY: stripeApiKey,
+      STRIPE_SECRET_KEY:  stripeApiSecret
     },
 };
 
